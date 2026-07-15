7cd90b6395cfe9d82b35bc533704af8c81ed976d
aaryansingh66665 <aaryansingh66665@users.noreply.github.com>
2026-07-15 14:56:55 +0530

    feat: embed project report inside website and add landing page report button

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-6ec9e3bb'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.uhqplogtt0s"
+    "revision": "0.c4e5oarolps"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
